dc96e468402280e29401acca58a0b9f7727c9a82
Armado Pagkalinawan Jr <armando.pagkalinawan@interactivepartners.com.au>
2018-07-12 01:28:39 +0800

    MySQLDBUpdate

--- Get_Wheels/getwheels/config.py
+++ Get_Wheels/getwheels/config.py
@@ -1,7 +1,7 @@
 
 class Config:
 	SECRET_KEY = '}I\xdf\xc6\xe0Pl3\xaf0\xd9\xe2\x80\xa2E\xc2'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql12247136:vxG4dWStWa@sql12.freemysqlhosting.net/sql12247136'
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
